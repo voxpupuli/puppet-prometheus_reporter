6b8c8a4d4da99ce87c0ac588c0d7bae71f63c2e3
Tim Meusel <tim@bastelfreak.de>
2023-08-18 08:34:30 +0200

    rubocop: autofix

--- lib/puppet/reports/prometheus.rb
+++ lib/puppet/reports/prometheus.rb
@@ -11,9 +11,7 @@ Puppet::Reports.register_report(:prometheus) do
   # lib/puppet/reports/graphite.rb
   configfile = File.join([File.dirname(Puppet.settings[:config]),
                           'prometheus.yaml'])
-  unless File.exist?(configfile)
-    raise(Puppet::ParseError, "Prometheus report config file #{configfile} not readable")
-  end
+  raise(Puppet::ParseError, "Prometheus report config file #{configfile} not readable") unless File.exist?(configfile)
 
   config = YAML.load_file(configfile)
 
@@ -23,16 +21,13 @@ Puppet::Reports.register_report(:prometheus) do
   REPORTS = config['reports']
   STALE_TIME = config['stale_time']
 
-  if TEXTFILE_DIRECTORY.nil? || !File.exist?(TEXTFILE_DIRECTORY)
-    raise(Puppet::ParseError, "#{configfile}: textfile_directory is not set or is missing.")
-  end
+  raise(Puppet::ParseError, "#{configfile}: textfile_directory is not set or is missing.") if TEXTFILE_DIRECTORY.nil? || !File.exist?(TEXTFILE_DIRECTORY)
 
-  unless REPORT_FILENAME.nil? || REPORT_FILENAME.end_with?('.prom')
-    raise(Puppet::ParseError, "#{configfile}: report_filename does not ends with .prom")
-  end
+  raise(Puppet::ParseError, "#{configfile}: report_filename does not ends with .prom") unless REPORT_FILENAME.nil? || REPORT_FILENAME.end_with?('.prom')
 
   def process
     return unless ENVIRONMENTS.nil? || ENVIRONMENTS.include?(environment)
+
     namevar = if REPORT_FILENAME.nil?
                 host + '.prom'
               else
@@ -54,27 +49,28 @@ Puppet::Reports.register_report(:prometheus) do
     unless metrics.empty? || metrics['events'].nil?
       metrics.each do |metric, data|
         next unless REPORTS.nil? || REPORTS.include?(metric)
+
         case metric
         when 'changes'
-          definitions << <<-EOS
-# HELP puppet_report_changes Changed resources in the last puppet run
-# TYPE puppet_report_changes gauge
-EOS
+          definitions << <<~EOS
+            # HELP puppet_report_changes Changed resources in the last puppet run
+            # TYPE puppet_report_changes gauge
+          EOS
         when 'events'
-          definitions << <<-EOS
-# HELP puppet_report_events Resource application events
-# TYPE puppet_report_events gauge
-EOS
+          definitions << <<~EOS
+            # HELP puppet_report_events Resource application events
+            # TYPE puppet_report_events gauge
+          EOS
         when 'resources'
-          definitions << <<-EOS
-# HELP puppet_report_resources Resources broken down by their state
-# TYPE puppet_report_resources gauge
-EOS
+          definitions << <<~EOS
+            # HELP puppet_report_resources Resources broken down by their state
+            # TYPE puppet_report_resources gauge
+          EOS
         when 'time'
-          definitions << <<-EOS
-# HELP puppet_report_time Resource apply times
-# TYPE puppet_report_time gauge
-EOS
+          definitions << <<~EOS
+            # HELP puppet_report_time Resource apply times
+            # TYPE puppet_report_time gauge
+          EOS
         end
         data.values.each do |val|
           new_metrics["puppet_report_#{metric}{name=\"#{val[1]}\",#{common_values.join(',')}}"] = val[2]
@@ -84,18 +80,18 @@ EOS
 
     epochtime = DateTime.now.new_offset(0).strftime('%Q').to_i / 1000.0
     new_metrics["puppet_report{#{common_values.join(',')}}"] = epochtime
-    definitions << <<-EOS
-# HELP puppet_report Unix timestamp of the last puppet run
-# TYPE puppet_report gauge
-EOS
+    definitions << <<~EOS
+      # HELP puppet_report Unix timestamp of the last puppet run
+      # TYPE puppet_report gauge
+    EOS
 
     if defined?(transaction_completed) && ([true, false].include? transaction_completed)
       completed = transaction_completed == true ? 1 : 0
       new_metrics["puppet_transaction_completed{#{common_values.join(',')}}"] = completed
-      definitions << <<-EOS
-# HELP puppet_transaction_completed transaction completed status of the last puppet run
-# TYPE puppet_transaction_completed gauge
-EOS
+      definitions << <<~EOS
+        # HELP puppet_transaction_completed transaction completed status of the last puppet run
+        # TYPE puppet_transaction_completed gauge
+      EOS
     end
 
     cached_catalog_state = [0, 0, 0]
@@ -111,10 +107,10 @@ EOS
       new_metrics["puppet_cache_catalog_status{state=\"not_used\",#{common_values.join(',')}}"] = cached_catalog_state[0]
       new_metrics["puppet_cache_catalog_status{state=\"explicitly_requested\",#{common_values.join(',')}}"] = cached_catalog_state[1]
       new_metrics["puppet_cache_catalog_status{state=\"on_failure\",#{common_values.join(',')}}"] = cached_catalog_state[2]
-      definitions << <<-EOS
-# HELP puppet_cache_catalog_status whether a cached catalog was used in the run, and if so, the reason that it was used
-# TYPE puppet_cache_catalog_status gauge
-EOS
+      definitions << <<~EOS
+        # HELP puppet_cache_catalog_status whether a cached catalog was used in the run, and if so, the reason that it was used
+        # TYPE puppet_cache_catalog_status gauge
+      EOS
     end
 
     # Set initial status
@@ -131,10 +127,10 @@ EOS
       new_metrics["puppet_status{state=\"failed\",#{common_values.join(',')}}"] = status_state[0]
       new_metrics["puppet_status{state=\"changed\",#{common_values.join(',')}}"] = status_state[1]
       new_metrics["puppet_status{state=\"unchanged\",#{common_values.join(',')}}"] = status_state[2]
-      definitions << <<-EOS
-# HELP puppet_status the status of the client run
-# TYPE puppet_status gauge
-EOS
+      definitions << <<~EOS
+        # HELP puppet_status the status of the client run
+        # TYPE puppet_status gauge
+      EOS
     end
 
     File.open(filename, 'w') do |file|
@@ -153,15 +149,14 @@ EOS
       end
     end
 
-    File.open(yaml_filename, 'w') do |yaml_file|
-      yaml_file.write new_metrics.to_yaml
-    end
+    File.write(yaml_filename, new_metrics.to_yaml)
 
     clean_stale_reports
   end
 
   def clean_stale_reports
     return if STALE_TIME.nil? || STALE_TIME < 1
+
     Dir.chdir(TEXTFILE_DIRECTORY)
     Dir.glob('*.prom').each { |filename| File.delete(filename) if (Time.now - File.mtime(filename)) / (24 * 3600) > STALE_TIME }
   end
